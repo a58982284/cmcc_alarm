398dc11d0c7e6b2a15bd78ade312304c679d1d69
a58982284 <a58982284@163.com>
2018-12-21 20:36:17 +0800

    12212035
    vm_moniter

--- vm_monitor.py
+++ vm_monitor.py
@@ -11,8 +11,8 @@ import ConfigParser
 import threading
 import keep_alive
 import evacuate_vm
-import traffic-create-mysql
-
+import MySQLdb
+import yaml
 from watchmen.common import enums
 from watchmen.common.fmevent import FmEvent
 from watchmen.producer.eventsender import EventSender
@@ -61,10 +61,22 @@ def main():
     region_status, region = commands.getstatusoutput(
         "grep \"region_name\" /etc/watchmen/watchmen-producer.conf | cut -d\"=\" -f2")
     commands.getstatusoutput("source /root/openrc")
-    # create the vm status file
+
+
     res = curl_get_kpi()
     res = res["servers"]
     tenant_id = []  # 里面有多个重复的tenant_id,用的时候取第一个就行了
+
+    mysql_root_password = get_mysql_password()
+    mysql_vip = "{{ fuel_network.management_vip }}"
+    db = MySQLdb.connect(mysql_vip, "root", mysql_root_password)
+    cursor = db.cursor()
+    create_dase_sql = """create database if not exists om_datafree;"""  # 判断有没有数据库
+    cursor.execute(create_dase_sql)
+    use_base_sql = """use om_datafree;"""
+    cursor.execute(use_base_sql)
+    create_table_sql = """create table if not exists vm_monitor (vm_uuid varchar(64) not null unique, vm_status varchar(16) not null);"""
+    cursor.execute(create_table_sql)
     a = {}  # 缓存文件
     for i in res:
         vm_uuid = i.get('id', '')
@@ -73,7 +85,14 @@ def main():
         tenant_id_vm = i.get('tenant_id', '')
         tenant_id.append(tenant_id_vm)
         a[vm_uuid] = vm_last_status
-    # 然后需要先判断一下sql中有没有这张表,如果没有的话就创建一张表
+        insert_sql = """insert into vm_monitor values('%s', %s) ON DUPLICATE KEY UPDATE %s;""" % (vm_uuid, vm_last_status,vm_last_status)  #往数据库里加入数据,如果存在则更新,如果不存在则插入
+        cursor.execute(insert_sql)
+        db.commit()
+        db.close()
+    # 然后需要先判断一下sql中有没有这张表,如果没有的话就创建一张表,如果有,就读取数据库的表放在a字典里
+    # 如果没有的话,创建一个表
+        # 判断数据库里有没有对应的表
+
 
     while True:
         res_two = curl_get_kpi()
@@ -156,7 +175,7 @@ def main():
                                 elif keep_Alive1 and evacuation:
                                     # evacuate the vm for a DOWN host
                                     evacuate_vm.main(k)
-                                elif keep_Alive1:
+                                elif keep_Alive1 and not evacuation:
                                     # keep alive
                                     keep_alive.main(k)
                     if a[k] == "ERROR" and b[k] == "ACTIVE":
@@ -193,11 +212,26 @@ def main():
                             "VM %s has changed to status ACTIVE" % (k)
                         )
                         evnet_sender4.create_new_fm_event(event4)
+            # 如果改动if a !=b,则将b写入mysql
+            if cmp (a, b) !=0:
+                mysql_root_password = get_mysql_password()
+                mysql_vip = "{{ fuel_network.management_vip }}"
+                db = MySQLdb.connect(mysql_vip, "root", mysql_root_password)
+                cursor = db.cursor()
+                use_base_sql = """use om_datafree;"""
+                cursor.execute(use_base_sql)
+                update_sql = """update vm_monitor set vm_status=%s where vm_uuid='%s';""" % (vm_curr_status, vm_uuid)
+                cursor.execute(update_sql)
+                db.commit()
+                db.close()
         a = b   # 覆盖a的缓存
-        #如果改动if a !=b,则将b写入mysql
-        time.sleep(1)
 
 
+def get_mysql_password():
+    with open('/etc/astute.yaml', 'r') as f:
+        y = yaml.load(f)
+    return y['mysql']['root_password']
 
 if __name__ == '__main__':
     main()
+    time.sleep(1)
